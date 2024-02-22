9d0e993e9e921b7c1cc6d2d0b664c1253dd58bd2
yoavstrugo <yoav@strugo.net>
2024-02-22 13:04:38 +0200

    Added the LiveImageDisplay class

---
+++ perfectswish/api/live_image_dsiplay.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+
+import cv2
+from PIL import Image, ImageTk
+
+FPS = 30
+WIDTH = 1920
+HEIGHT = 1080
+
+
+class LiveImageDisplay:
+    def __init__(self, main_loop, *args, framerate=FPS, width=WIDTH, height=HEIGHT):
+        self.main_loop = main_loop
+        self.args = args
+
+        self.delay = 1000 // framerate
+        self.width = width
+        self.height = height
+
+        self.images = []
+
+        self.root = tk.Tk()
+        self.root.title("Image Display")
+
+        self.canvas = tk.Canvas(self.root, width=self.width, height=self.height)
+        self.canvas.pack()
+
+    def run(self):
+        images = self.main_loop(*self.args)
+        self.display_image(images)
+        self.root.after(self.delay, self.run)
+        self.root.update()
+
+    def display_image(self, images):
+        self.images = []
+        # check if images is iterable and display all images
+        if images is not None and hasattr(images, '__iter__'):
+            # display multiple image
+            image_width = self.width // len(images)
+            for i, image in enumerate(images):
+                image_pos = i * image_width
+                image_resized = cv2.resize(image, (image_width, self.height), interpolation=cv2.INTER_AREA)
+                image_tk = ImageTk.PhotoImage(image=Image.fromarray(image_resized))
+                self.canvas.create_image(image_pos, 0, anchor=tk.NW, image=image_tk)
+
+                self.images.append(image_tk)
+            return
+
+        # display single image
+        image_resized = cv2.resize(images, (self.width, self.height))
+        image_tk = ImageTk.PhotoImage(image=Image.fromarray(image_resized))
+        self.canvas.create_image(0, 0, anchor=tk.NW, image=image_tk)
+        self.canvas.image = image_tk
