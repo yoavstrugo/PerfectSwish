d1d6e6bed4fe1478abfbd1edb737dfa181f7738b
yoavstrugo <yoav@strugo.net>
2024-02-22 11:18:40 +0200

    Added project structure

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="PerfectSwish",
+    version="0.1.0",
+    description="A short description of my project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    install_requires=required,
+    python_requires='>=3.10',
+)
