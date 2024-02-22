62eb6e0eac00dfa7d5f193867531249646cb57ce
yoavstrugo <yoav@strugo.net>
2024-02-22 15:02:30 +0200

    add auto create data dir

--- perfectswish/api/data_io.py
+++ perfectswish/api/data_io.py
@@ -2,13 +2,19 @@ from pathlib import Path
 
 import numpy as np
 
+__DATA_DIR = Path.home() / ".perfectswish"
 
 def get_data_dir() -> Path:
     """
     Returns the data directory for the project.
     :return:
     """
-    return Path.home() / ".perfectswish"
+    data_dir = __DATA_DIR
+
+    if not data_dir.exists():
+        data_dir.mkdir()
+
+    return data_dir
 
 
 def np_load(file_name):
