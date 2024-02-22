eeba288b48a589fdba01b4784e6a61578eee2fa1
yoavstrugo <yoav@strugo.net>
2024-02-22 12:20:07 +0200

    Created Color type, and moved Colors to utils class

--- perfectswish/api/common_objects.py
+++ perfectswish/api/common_objects.py
@@ -2,20 +2,7 @@ from dataclasses import dataclass
 
 import numpy as np
 
-
-class Color:
-    """
-    Represents the color of a ball in the pool game.
-    """
-    WHITE = (255, 255, 255)
-    YELLOW = (0, 255, 255)
-    BLUE = (255, 0, 0)
-    RED = (0, 0, 255)
-    PURPLE = (255, 0, 255)
-    ORANGE = (0, 165, 255)
-    GREEN = (0, 255, 0)
-    BROWN = (42, 42, 165)
-    BLACK = (0, 0, 0)
+from perfectswish.api.utils import Color, Colors
 
 
 @dataclass
@@ -33,7 +20,7 @@ class Ball:
     position: np.array
     stripped: bool = False
     radius: int = 15
-    color: Color = Color.WHITE
+    color: Color = Colors.WHITE
     in_pocket: bool = False
 
 
---
+++ perfectswish/api/utils.py
@@ -0,0 +1,17 @@
+Color = tuple[int, int, int]
+
+
+class Colors:
+    """
+    Predefined colors, in BGR.
+    """
+    WHITE: Color = (255, 255, 255)
+    YELLOW: Color = (0, 255, 255)
+    BLUE: Color = (255, 0, 0)
+    RED: Color = (0, 0, 255)
+    PURPLE: Color = (255, 0, 255)
+    ORANGE: Color = (0, 165, 255)
+    GREEN: Color = (0, 255, 0)
+    BROWN: Color = (42, 42, 165)
+    BLACK: Color = (0, 0, 0)
+
