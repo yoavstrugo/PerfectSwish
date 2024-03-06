774996a97b0c6e5f39913e56030293c3522b5c03
TLP <itay.shtober.44@gmail.com>
2024-03-06 18:13:29 +0200

    creating the main function: get_avarage_balls_list

--- perfectswish/object_detection/balls_locations_memory.py
+++ perfectswish/object_detection/balls_locations_memory.py
@@ -7,22 +7,25 @@ from tqdm import tqdm as wassach
 from perfectswish.object_detection.detect_objects import ball_objects
 
 
-def return_balls_list(recent_balls_list: List[List[Ball]], id_to_ball: dict[int, Ball], last_frame_num = 10, last_frame_percent = 0) -> List[Ball]:
+def return_balls_list(recent_balls_list: List[List[Ball]], id_to_ball: dict[float, Ball], last_frame_num=10,
+                      last_frame_percent=0) -> List[Ball]:
     """
-    This function gets the 30 most recent balls list and num of last frame, percent of them and return
+    This function gets the most recent balls list and num of last frame, percent of them and return
     a list of the balls that will appear on the board
     """
     recent = recent_balls_list[-last_frame_num:]
     balls_times_dict = {}
     for frame in recent:
         for ball in frame:
+            found = False
             for ball_2_id in balls_times_dict.keys():
                 ball_2 = id_to_ball[ball_2_id]
-                if np.linalg.norm(ball.position - ball_2.position) < 50 and ball.id != ball_2.id:
-                    print(f"ball {ball.id} is close to ball {ball_2.id}")
-                    balls_times_dict[ball.id] += 1
+                if np.linalg.norm(ball.position - ball_2.position) < 10 and ball.id != ball_2.id:
+                    balls_times_dict[ball_2.id] += 1
+                    found = True
                     break
-            balls_times_dict[ball.id] = 1
+            if not found:
+                balls_times_dict[ball.id] = 1
     id_for_delete = []
     for id, times in balls_times_dict.items():
         if times < last_frame_percent * last_frame_num:
@@ -56,19 +59,35 @@ def find_balls_list_and_avarage_list():
 
     balls_list_and_avarge_list = []
 
-    i=0
+    i = 0
     for ball_list in wassach(recent_balls_list, desc="Running the function on the recent balls list"):
-        if i<10:
-            i+=1
+        if i < 10:
+            i += 1
             continue
-        balls_list_and_avarge_list.append([ball_list, return_balls_list(recent_balls_list[:i], id_to_ball, 10, 7)])
-        i+=1
+        balls_list_and_avarge_list.append([ball_list, return_balls_list(recent_balls_list[:i], id_to_ball, 15, 0.7)])
+        i += 1
 
     return balls_list_and_avarge_list
 
 
+# balls_list_and_avarge_list = find_balls_list_and_avarage_list()
+# for i in range(len(balls_list_and_avarge_list)):
+#     print(len(balls_list_and_avarge_list[i][1]))
 
-balls_list_and_avarge_list = find_balls_list_and_avarage_list()
-eighty_place = balls_list_and_avarge_list[15]
 
-
+def get_avarage_balls_list(balls_lists: List[List[Ball]]) -> List[Ball]:
+    """
+    This function gets a list of balls lists and return the avarge of the balls that will appear on the board
+    :param balls_lists: List of balls lists, the last frames
+    :return: List of balls for showing on the board
+    """
+    last_frame_num = len(balls_lists)
+    i = 0
+    id_to_ball = {}
+    for frame in balls_lists:
+        for ball in frame:
+            ball.id = i
+            id_to_ball[i] = ball
+            i += 1
+    showing_balls_list = return_balls_list(balls_lists, id_to_ball, last_frame_num, 0.7)
+    return showing_balls_list
