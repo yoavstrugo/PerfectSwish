3291f34001e8c9417c9613ee3779bf7ba21744af
yoavstrugo <yoav@strugo.net>
2024-06-10 10:19:42 +0300

    added connect points and points in relation to + documented

--- perfectswish/new_image_transformation/points_frame_decorator.py
+++ perfectswish/new_image_transformation/points_frame_decorator.py
@@ -10,23 +10,36 @@ class Points(FrameDecorator):
     static, should be used as a decorator to add data for other decorator depending on the points.
     """
 
-    # TODO: min imaeg height width for resize
-
-    def __init__(self, frame, initial_points: list = None, render_points: bool = True,
-                 radius_tolerance: int = 10, point_color: (int, int, int) = (0, 0, 255),
+    def __init__(self, frame, initial_points: list = None, points_in_relation_to: tuple = None,
+                 render_points: bool = True, radius_tolerance: int = 10, connected_points: bool = False,
+                 point_color: (int, int, int) = (0, 0, 255),
                  selected_point_color: (int, int, int) = (0, 255, 0),
-                 point_radius: int = 3, point_padding: int = 2, pass_points: list[Callable] = None, **kwargs):
+                 point_radius: int = 3, point_padding: int = 2, pass_points: list[Callable] = None):
+        """
+        Initiate the Points frame decorator.
+        :param frame: The frame to decorate.
+        :param initial_points: Initial points to set on the canvas, in the coords of the image or in the
+        coords of points_in_relation_to, if given.
+        :param points_in_relation_to: Pass if the initial points are in the coords of another image.
+        :param render_points: Whether to render the points on the canvas.
+        :param radius_tolerance: Points will be selected if the user clicks on them within this radius.
+        :param connected_points: Whether to connect the points with lines.
+        :param point_color: The color of the points.
+        :param selected_point_color: The color of the selected point.
+        :param point_radius: The radius of the points.
+        :param point_padding: The padding of the points from the edge of the canvas.
+        :param pass_points: A list of functions that will be called with the points in the image coords.
+        """
         # Initiate the points
-        super().__init__(frame, **kwargs)
-        self._set_points(initial_points or [])
+        super().__init__(frame)
+        self.__points_in_relation_to = points_in_relation_to
+        self._set_points(initial_points or [], points_in_relation_to=points_in_relation_to)
 
         self._pass_points = pass_points or []
 
         self._transform_point: TransformationPipline[(int, int)] = TransformationPipline()
         self._selected_point = None
-        # if render_points:
-        #     self._transform_image.compose(self.__get_image_with_points, last=True)
-
+        self.__connected_points = connected_points
         self.__point_color = point_color
         self.__selected_point_color = selected_point_color
         self.__point_radius = point_radius
@@ -37,16 +50,31 @@ class Points(FrameDecorator):
 
         self.__bind_events()
 
-    def _set_points(self, points: list):
+    def _set_points(self, points: list, points_in_relation_to: tuple = None) -> None:
+        """
+        This function will set the points, the list given is 'image' (or points_in_relation points, if given).
+        :param points: Points in the coords of the image, or in the coords of points_in_relation_to, if given.
+        """
         self._points = points
         self._max_points = len(self._points)
-        self._points = [self._get_canvas_point(x, y) for x, y in self._points]
+        if points_in_relation_to is None:
+            self._points = [self._get_canvas_point(x, y) for x, y in self._points]
+        else:
+            self._points = [
+                self.convert_point_coords(x, y, points_in_relation_to, (self._width, self._height))
+                for x, y in self._points]
 
     def __bind_events(self):
-        # Bind the click event
+        """
+        This function will bind the events to the canvas.
+        """
         self._canvas.bind("<Button-1>", self.__on_mouse_press, add=True)
 
     def __select_point(self, px, py):
+        """
+        This function will select a point if it's close enough to the given coords.
+        :return:
+        """
         for i, (x, y) in enumerate(self._points):
             if abs(px - x) < self.__radius_tolerance and abs(py - y) < self.__radius_tolerance:
                 self._selected_point = i
@@ -55,10 +83,14 @@ class Points(FrameDecorator):
         return None
 
     def __on_mouse_press(self, event):
+        """
+        This function will be called when the user clicks on the canvas. it will select the point if the user
+        clicked on (next) a point.
+        """
         self.__select_point(event.x, event.y)
 
     @staticmethod
-    def convert_point_coords(x, y, src_coords, dst_coords) -> tuple:
+    def convert_point_coords(x: int, y: int, src_coords: tuple, dst_coords: tuple) -> tuple:
         """
         Convert point x,y from a source coords to destination coords (proportionally, if point was sampled
         in context of 1920x1080, and you want to convert it to 1280x720, you should pass the source coords as
@@ -68,19 +100,24 @@ class Points(FrameDecorator):
         scale_y = dst_coords[1] / src_coords[1]
         return x * scale_x, y * scale_y
 
-    def _get_canvas_point(self, x, y):
+    def _get_canvas_point(self, x: int, y: int) -> tuple:
         """
         This function will return the canvas point from the image point.
         """
-        return self.convert_point_coords(x, y, (self._img_orig_width, self._img_orig_height), (self._width, self._height))
+        return self.convert_point_coords(x, y, (self._img_orig_width, self._img_orig_height),
+                                         (self._width, self._height))
 
-    def _get_image_point(self, x, y):
+    def _get_image_point(self, x: int, y: int) -> tuple:
         """
         This function will return the image point from the canvas point.
         """
-        return self.convert_point_coords(x, y, (self._width, self._height), (self._img_orig_width, self._img_orig_height))
+        return self.convert_point_coords(x, y, (self._width, self._height),
+                                         (self._img_orig_width, self._img_orig_height))
 
-    def __draw_points(self):
+    def __draw_points(self) -> None:
+        """
+        This function will draw the points on the canvas.
+        """
         for pass_points_func in self._pass_points:
             image_points = [self._get_image_point(x, y) for x, y in self._points]
             pass_points_func(image_points)
@@ -92,6 +129,7 @@ class Points(FrameDecorator):
             else:
                 color = self.__point_color
 
+            # make sure the points are not out of the canvas
             if x - self.__point_radius < 0:
                 x = max(x, self.__point_radius + self.__point_padding)
             elif x + self.__point_radius > self._width:
@@ -102,10 +140,12 @@ class Points(FrameDecorator):
             elif y + self.__point_radius > self._height:
                 y = min(y, self._height - self.__point_radius - self.__point_padding)
 
+            # draw points
             self._canvas.create_oval(x - self.__point_radius, y - self.__point_radius,
                                      x + self.__point_radius, y + self.__point_radius,
                                      fill='#%02x%02x%02x' % color)
 
+            # draw points text, some trail-and-error to make it look good
             text_x, text_y = x + 5, y + 5
             if x <= self._width / 2 and y <= self._height / 2:
                 text_x, text_y = x + 8, y + 8
@@ -117,3 +157,11 @@ class Points(FrameDecorator):
                 text_x, text_y = x - 8, y - 8
 
             self._canvas.create_text(text_x, text_y, text=str(i + 1), fill='#%02x%02x%02x' % color)
+
+        # draw lines between points
+        if self.__connected_points:
+            for i in range(len(self._points) - 1, -1, -1):
+                x1, y1 = self._points[i]
+                x2, y2 = self._points[i - 1]
+                self._canvas.create_line(x1, y1, x2, y2, fill='#%02x%02x%02x' % self.__point_color)
+
