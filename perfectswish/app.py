63caea4b896c80f9aca52db130c44eb544b9e472
yoavstrugo <yoav@strugo.net>
2024-06-10 10:31:17 +0300

    using global settings and updated phase switch decorator to change geometry

--- perfectswish/app.py
+++ perfectswish/app.py
@@ -1,10 +1,11 @@
 import tkinter as tk
 
-from perfectswish.utils import data_io, webcam
-from perfectswish.utils.monitor_utils import get_other_screen, get_root_screen
 from perfectswish.phases.crop_phase import CropPhase
 from perfectswish.phases.game_phase import GamePhase
 from perfectswish.phases.projection_align_phase import ProjectAlignPhase
+from perfectswish.settings import DEFAULT_APP_GEOMETRY
+from perfectswish.utils import data_io, webcam
+from perfectswish.utils.monitor_utils import get_other_screen, get_root_screen
 
 DATA_FILE = 'app_data.dat'
 CAMERA = 0
@@ -14,12 +15,13 @@ class PerfectSwishApp(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Perfect Swish")
-        self.geometry("800x600")
+        self.geometry(DEFAULT_APP_GEOMETRY)
 
         self.__frames: list[tuple[tk.Frame, tk.Toplevel | None]] = []
         self.__cached_data = None
         self.load_data()
-        self._webcam_capture = webcam.WebcamCapture(CAMERA, 1920, 1080, 24)
+        self._webcam_capture = webcam.WebcamCapture(
+            r'C:\Users\TLP-266\PyCharmProject\PerfectSwish\videos\full_balls_no_green.mp4', 1920, 1080, 24)
         self.resizable(False, False)
 
         self._phase = None
@@ -30,16 +32,25 @@ class PerfectSwishApp(tk.Tk):
         self.crop_phase()
 
     @staticmethod
-    def phase_switch(func):
-        def wrapper(self, *args, **kwargs):
-            if self._phase:
-                name = self._phase.name
-                data = self._phase.get_data()
-                self.save_data(name, data)
-                self._phase.destroy()
-            func(self, *args, **kwargs)
+    def phase_switch(gemoetry=DEFAULT_APP_GEOMETRY):
+        def decorator(func):
+            def wrapper(self, *args, **kwargs):
+                if self._phase:
+                    name = self._phase.name
+                    data = self._phase.get_data()
+                    self.save_data(name, data)
+                    self._phase.destroy()
+                self.geometry(gemoetry)
+                func(self, *args, **kwargs)
 
-        return wrapper
+            return wrapper
+
+        if callable(gemoetry):
+            func = gemoetry
+            gemoetry = DEFAULT_APP_GEOMETRY
+            return decorator(func)
+        else:
+            return decorator
 
     def quit(self):
         if self._phase:
@@ -68,7 +79,8 @@ class PerfectSwishApp(tk.Tk):
     @phase_switch
     def crop_phase(self):
         data = self.__cached_data.get('crop')
-        self._phase = CropPhase(self, data, self.project_align_phase, None, self._webcam_capture)
+        self._phase = CropPhase(self, data, self.project_align_phase, None,
+                                self._webcam_capture)
 
     @phase_switch
     def project_align_phase(self):
@@ -81,8 +93,8 @@ class PerfectSwishApp(tk.Tk):
     def game_phase(self):
         crop_data = self.__cached_data.get('crop')
         project_data = self.__cached_data.get('project_align')
-        self._phase = GamePhase(self, crop_data, project_data, self._webcam_capture, self.__other_screen,
-                                None, None)
+        self._phase = GamePhase(self, crop_data, project_data, self._webcam_capture.create_multiprocess(),
+                                self.__other_screen, fps=24, balls_update_rate=24)
 
 
 if __name__ == '__main__':
