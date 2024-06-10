9a39268c91b2543487beb9905e6f9e0009c3d2c9
yoavstrugo <yoav@strugo.net>
2024-06-10 10:19:55 +0300

    added global settings

---
+++ perfectswish/settings.py
@@ -0,0 +1,27 @@
+from collections import namedtuple
+
+RESOLUTION_FACTOR = 8
+BOARD_BASE_WIDTH = 112
+BOARD_BASE_HEIGHT = 224
+
+Size = namedtuple('Size', ['width', 'height'])
+BOARD_SIZE = Size(width=BOARD_BASE_WIDTH * RESOLUTION_FACTOR, height=BOARD_BASE_HEIGHT * RESOLUTION_FACTOR)
+
+DEFAULT_APP_GEOMETRY = "800x600"
+
+__DEBUG_FLAG = False
+
+
+
+
+def set_flag():
+    global __DEBUG_FLAG
+    __DEBUG_FLAG = True
+
+
+def get_flag():
+    global __DEBUG_FLAG
+    if __DEBUG_FLAG:
+        __DEBUG_FLAG = False
+        return True
+    return False
