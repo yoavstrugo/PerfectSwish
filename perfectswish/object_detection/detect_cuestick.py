2368f35cb19d109ee38b5cbca161a0cc1de1662b
Ron Sendrovich <ron.sendrovich.44@gmail.com>
2024-06-13 15:12:44 +0300

    blah

--- perfectswish/object_detection/detect_cuestick.py
+++ perfectswish/object_detection/detect_cuestick.py
@@ -15,7 +15,7 @@ MARKER_SIZE = 400
 def save_aruco_markers(n=10, filename="markers"):
     # Generating Unique Markers and placing them in a plt grid
     markers = []
-    for i in range(n+OFFSET):
+    for i in range(n + OFFSET):
         markers.append(aruco.generateImageMarker(marker_dict, i, MARKER_SIZE))
     markers = markers[OFFSET:]
 
@@ -43,6 +43,8 @@ class CuestickDetector:
         self.fiducial_to_stickend_ratio = fiducial_to_stickend_ratio
         self.back_fiducial_id = back_fiducial_id
         self.front_fiducial_id = front_fiducial_id
+        self.back_fiducial_center_coords = np.array([0, 0])
+        self.front_fiducial_center_coords = np.array([0, 0])
 
     def detect_cuestick(self, frame, return_corners=False):
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -58,27 +60,37 @@ class CuestickDetector:
                 elif ids[0] == self.front_fiducial_id:
                     front_fiducial_center = np.mean(corners, axis=0)
             if back_fiducial_center is not None and front_fiducial_center is not None:
-                stickend = back_fiducial_center * (
-                        self.fiducial_to_stickend_ratio + 1) - front_fiducial_center * self.fiducial_to_stickend_ratio
+                self.back_fiducial_center_coords = back_fiducial_center
+                self.front_fiducial_center_coords = front_fiducial_center
+                stickend = self.back_fiducial_center_coords * (
+                        self.fiducial_to_stickend_ratio + 1) - self.front_fiducial_center_coords * self.fiducial_to_stickend_ratio
                 if return_corners:
-                    return stickend, back_fiducial_center, front_fiducial_center, marker_corners
-                return stickend, back_fiducial_center, front_fiducial_center
+                    return stickend, self.back_fiducial_center_coords, self.front_fiducial_center_coords, marker_corners
+                return stickend, self.back_fiducial_center_coords, self.front_fiducial_center_coords
         return None
 
-    def draw_cuestick(self, frame, stickend, back_fiducial_center, front_fiducial_center):
+    def cover_aruco_markers(self, frame, radius=70):
+        cv2.circle(frame, tuple(np.int32(self.back_fiducial_center_coords)), radius, (150, 200, 100), -1)
+        cv2.circle(frame, tuple(np.int32(self.front_fiducial_center_coords)), radius, (150, 200, 100), -1)
+        return frame
+
+    def draw_cuestick(self, frame):
         cv2.line(
             frame,
-            tuple(back_fiducial_center.astype(int).ravel()),
-            tuple(front_fiducial_center.astype(int).ravel()),
+            tuple(self.back_fiducial_center_coords.astype(int).ravel()),
+            tuple(self.front_fiducial_center_coords.astype(int).ravel()),
             (0, 255, 0),
             4,
             cv2.LINE_AA,
         )
 
+        stickend = self.back_fiducial_center_coords * (
+                self.fiducial_to_stickend_ratio + 1) - self.front_fiducial_center_coords * self.fiducial_to_stickend_ratio
         cv2.circle(frame, tuple(stickend.astype(int).ravel()), 10, (0, 0, 255), -1)
 
-        vector = back_fiducial_center - front_fiducial_center
-        vector = vector / np.linalg.norm(vector)
+        vector = self.back_fiducial_center_coords - self.front_fiducial_center_coords
+        if np.linalg.norm(vector) != 0:
+            vector = vector / np.linalg.norm(vector)
         cv2.arrowedLine(
             frame,
             tuple(stickend.astype(int).ravel()),
@@ -98,9 +110,8 @@ if __name__ == '__main__':
         if not ret:
             break
         cuestick = detector.detect_cuestick(frame)
-        if cuestick is not None:
-            stickend, back_fiducial_center, front_fiducial_center = cuestick
-            frame = detector.draw_cuestick(frame, stickend, back_fiducial_center, front_fiducial_center)
+        frame = detector.draw_cuestick(frame)
+        covered_frame = detector.cover_aruco_markers(frame)
         cv2.imshow("Cuestick Detection", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -110,4 +121,3 @@ if __name__ == '__main__':
 # if __name__ == '__main__':
 #     save_aruco_markers(n=4, filename="large_markers")
 #
-
